1dacd5717d94335d0de005afbdb6810491d9d8b8
Benoit Billington <benoit@afrogleap.com>
2014-04-15 13:38:56 +0200

    Improve the Demo

--- demo/src/main/java/be/billington/calendar/recurrencepicker/demo/activity/DemoActivity.java
+++ demo/src/main/java/be/billington/calendar/recurrencepicker/demo/activity/DemoActivity.java
@@ -5,13 +5,15 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import be.billington.calendar.recurrencepicker.EventRecurrence;
+import be.billington.calendar.recurrencepicker.EventRecurrenceFormatter;
 import be.billington.calendar.recurrencepicker.RecurrencePickerDialog;
 import be.billington.calendar.recurrencepicker.demo.R;
 
 public class DemoActivity extends FragmentActivity {
 
     private TextView recurrence;
-
+    private String recurrenceRule;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +21,34 @@ public class DemoActivity extends FragmentActivity {
 
         recurrence = (TextView) findViewById(R.id.recurrence);
 
+
         recurrence.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                RecurrencePickerDialog dialog = new RecurrencePickerDialog();
-                dialog.setOnRecurrenceSetListener(new RecurrencePickerDialog.OnRecurrenceSetListener() {
+            public void onClick(View v) {
+                RecurrencePickerDialog recurrencePickerDialog = new RecurrencePickerDialog();
+
+                if (recurrenceRule != null && recurrenceRule.length() > 0) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString(RecurrencePickerDialog.BUNDLE_RRULE, recurrenceRule);
+                    recurrencePickerDialog.setArguments(bundle);
+                }
+
+                recurrencePickerDialog.setOnRecurrenceSetListener(new RecurrencePickerDialog.OnRecurrenceSetListener() {
                     @Override
                     public void onRecurrenceSet(String rrule) {
+                        recurrenceRule = rrule;
 
+                        if (recurrenceRule != null && recurrenceRule.length() > 0) {
+                            EventRecurrence recurrenceEvent = new EventRecurrence();
+                            recurrenceEvent.parse(rrule);
+                            String srt = EventRecurrenceFormatter.getRepeatString(DemoActivity.this, getResources(), recurrenceEvent, true);
+                            recurrence.setText(srt);
+                        } else {
+                            recurrence.setText("No recurrence");
+                        }
                     }
                 });
-                dialog.show(getSupportFragmentManager(), "recurrencePicker");
+                recurrencePickerDialog.show(getSupportFragmentManager(), "recurrencePicker");
             }
         });
     }
